2058e60a4c12f4a7730f2101c43e447f0d44dfd3
Samuel Raphael <sammysgame.dev@gmail.com>
2021-05-21 16:25:26 +0100

    binary search with loops

---
+++ src/main/java/ng/com/asciidev/binarysearch/BinarySearch.java
@@ -0,0 +1,19 @@
+package ng.com.asciidev.binarysearch;
+
+import java.util.List;
+
+public class BinarySearch {
+    public static int binarySearchLoops(int target, List<Integer> array) {
+        int low = 0, high = array.size() - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (array.get(mid) < target)
+                low = mid + 1;
+            else if (array.get(mid) > target)
+                high = mid - 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+}
